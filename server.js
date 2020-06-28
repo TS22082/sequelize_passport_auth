4b043638ff77f106bc86b18ec8e1b7f8cf153949
TS22082 <ts22082@gmail.com>
2020-06-28 15:24:54 -0700

    setup basic routes

--- server.js
+++ server.js
@@ -5,4 +5,10 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const apiRoutes = require("./routes/api-routes");
+app.use(apiRoutes);
+
+const clientRoutes = require("./routes/client-routes");
+app.use(clientRoutes);
+
 app.listen(PORT, () => console.log(`Listening at: http://localhost:${PORT}`));
